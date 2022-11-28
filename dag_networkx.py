974b8f12b5b9f2a856d80a889abde807c6a16223
aisu-programming <aisu.programming@gmail.com>
2022-11-28 17:44:24 +0800

    Add function: prune DAG path

--- dag_networkx.py
+++ dag_networkx.py
@@ -1,41 +1,90 @@
 ''' Libraries '''
+import sys
 import time
+import math
 import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
 
 ''' Functions '''
-def plot_DAG(edges, number_of_nodes, filename=None, test=False):
+def plot_DAG(graph_data, filename=None, test=False):
+
+    node_num           = graph_data["node_num"]
+    edges              = graph_data["edges"]
+    isolated_node_ids  = graph_data["isolated_node_ids"]
+    source_ids         = graph_data["source_ids"]
+    removed_source_ids = graph_data["removed_source_ids"]
+    sink_ids           = graph_data["sink_ids"]
+    removed_sink_ids   = graph_data["removed_sink_ids"]
 
     G = nx.MultiDiGraph()
-    G.add_nodes_from(list(range(number_of_nodes)))
-    G.add_edges_from(edges)
+    G.add_nodes_from(list(range(node_num)))
+    for edge in edges: G.add_edge(edge[0], edge[1], weight=edge[2])
 
-    sources  = [ node for node,  in_degree in G.in_degree()  if  in_degree == 0 ]  # type: ignore
-    sinks    = [ node for node, out_degree in G.out_degree() if out_degree == 0 ]  # type: ignore
-    isolated = list(filter(lambda node: node in sinks, sources))
-    for node in isolated:
-        sources.remove(node)
-        sinks.remove(node)
+    # sources  = [ node for node,  in_degree in G.in_degree()  if  in_degree == 0 ]  # type: ignore
+    # sinks    = [ node for node, out_degree in G.out_degree() if out_degree == 0 ]  # type: ignore
+    # isolated = list(filter(lambda node: node in sinks, sources))
+    # for node in isolated:
+    #     sources.remove(node)
+    #     sinks.remove(node)
 
-    color_map = [
-        "yellow" if node in sources else
-            "cyan" if node in sinks else
-                "white" if node in isolated else "green" for node in G ]
+    # all_removed_sources = []
+    # while sources != [ 0 ]:
+    #     remove_sources = sources.copy()
+    #     remove_sources.remove(0)
+    #     G.remove_nodes_from(remove_sources)
+    #     all_removed_sources += remove_sources
+    #     sources  = [ node for node, in_degree in G.in_degree()  if  in_degree == 0 ]  # type: ignore
+    # G.add_nodes_from(all_removed_sources)
+
+    # all_removed_sinks = []
+    # while sinks != [ node_num-1 ]:
+    #     remove_sinks = sinks.copy()
+    #     remove_sinks.remove(node_num-1)
+    #     G.remove_nodes_from(remove_sinks)
+    #     all_removed_sinks += remove_sinks
+    #     sinks = [ node for node, out_degree in G.out_degree() if out_degree == 0 ]  # type: ignore
+    # G.add_nodes_from(all_removed_sinks)
+
+    color_map = []
+    for node_id in G:
+        if   node_id in source_ids        : color_map.append("lime")
+        elif node_id in removed_source_ids: color_map.append("green")
+        elif node_id in sink_ids          : color_map.append("cyan")
+        elif node_id in removed_sink_ids  : color_map.append("blue")
+        elif node_id in isolated_node_ids : color_map.append("red")
+        else                              : color_map.append("white")
+
+    node_positions = {}
+    for node_id in G:
+        position_tuple = [math.sin(node_id/node_num*2*math.pi),
+                          math.cos(node_id/node_num*-2*math.pi)]
+        if node_id in removed_source_ids+removed_sink_ids+isolated_node_ids:
+            position_tuple[0] *= 1.1
+            position_tuple[1] *= 1.1
+        node_positions[node_id] = position_tuple
+
+    edge_weights = list(nx.get_edge_attributes(G, "weight").values())
+    ews = edge_weights
+    edge_colors = [ (ew-min(ews))/(max(ews)-min(ews)) for ew in ews ]
+    edge_colors = [ str((1-ec)*(4/5)) for ec in edge_colors ]
+
     plt.figure(figsize=(10, 10))
     t1 = time.time()
+    print("Rendering... ", end='')
+    sys.stdout.flush()
     nx.draw(G,
-        pos=nx.kamada_kawai_layout(G, scale=2),
-        # pos=nx.circular_layout(G),
-        # pos=nx.spring_layout(G, scale=2),
         node_color=color_map,
+        pos=node_positions,
+        edge_color=edge_colors,
+        # width=2,
         edgecolors="black",
         with_labels=True,
         connectionstyle="arc3, rad=0.3"
     )
     t2 = time.time()
-    print(f"Rendering time: {t2-t1:.2f} seconds")
+    print(f"cost time: {t2-t1:.2f} seconds")
 
     filename = "test" if test else ("DAG" if filename is None else filename)
     plt.savefig(filename)
@@ -47,7 +96,7 @@ def plot_DAG(edges, number_of_nodes, filename=None, test=False):
 ''' Execution '''
 if __name__ == "__main__":
 
-    NUMBER_OF_NODES = 250
+    NUMBER_OF_NODES = 64
     CONNECTIONS_PER_NODE = 3
 
     node_ids = list(range(1, NUMBER_OF_NODES+1))
--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 ''' Libraries '''
-import sys
 import torch
 import torch.nn as nn
 from tqdm import tqdm
@@ -16,6 +15,7 @@ def cycle_check(edges, input_amounts):
     if input_amounts_copy == [-1]*len(input_amounts_copy): return True
     else: return False
 
+
 class Model(nn.Module):
     def __init__(self, node_num, max_depth, threshold_of_connection,
                  max_number_of_connection, min_number_of_connection):
@@ -26,7 +26,10 @@ class Model(nn.Module):
                  max_number_of_connection, min_number_of_connection)
             for node_id in range(node_num) ]
         self.alphas = torch.rand(node_num*node_num)
-        for i in range(node_num): self.alphas[i*node_num+i] = 0
+        for i in range(node_num):
+            self.alphas[i*node_num+i] = 0  # prevent node from linking to itself
+            self.alphas[i*node_num] = 0  # Make node 0 source
+            self.alphas[node_num*(node_num-1)+i] = 0  # Make node 0 source
         self.t_c     = threshold_of_connection
         self.max_n_c = max_number_of_connection
         self.min_n_c = min_number_of_connection
@@ -44,15 +47,25 @@ class Model(nn.Module):
             to_node_id   = max_alpha_id  % self.node_num
             connection_limit = self.max_n_c if max_alpha > self.t_c else self.min_n_c
 
-            # Limitation
+            # Limitation to output
             edges_to_node = list(filter(lambda e: e[1]==to_node_id, edges))
             if len(edges_to_node) >= connection_limit:
                 for from_node_id in range(self.node_num):
                     alphas_tmp[from_node_id*self.node_num+to_node_id] = 0
                 continue
+            # Limitation to input
+            edges_from_node = list(filter(lambda e: e[0]==from_node_id, edges))
+            if len(edges_from_node) >= connection_limit:
+                for to_node_id in range(self.node_num):
+                    alphas_tmp[from_node_id*self.node_num+to_node_id] = 0
+                continue
+            # Only limit to output --> create multiple sinks but only one source
+            # Only limit to input --> create multiple sources but only one sink
+            # Both --> create multiple sources and multiple sinks
+            # Do nothing --> create only one source and only one sink
 
             edges_tmp, input_amounts_tmp = edges.copy(), input_amounts.copy()
-            edges_tmp.append((from_node_id, to_node_id))
+            edges_tmp.append((from_node_id, to_node_id, max_alpha))
             if input_amounts_tmp[from_node_id] == -1: input_amounts_tmp[from_node_id] = 0
             input_amounts_tmp[to_node_id] = input_amounts_tmp[to_node_id] + 1 if input_amounts_tmp[to_node_id] != -1 else 1
             if cycle_check(edges_tmp, input_amounts_tmp):
@@ -63,8 +76,43 @@ class Model(nn.Module):
             else:
                 alphas_tmp[max_alpha_id] = 0
             pbar.update(1)
-        self.edges = edges
         pbar.close()
+        self.edges:list[int] = edges
+        self.isolated_node_ids = [ node_id for node_id in range(self.node_num) if node_id not in self.from_node_ids and node_id not in self.to_node_ids ]
+
+    @property
+    def from_node_ids(self) -> list[int]:
+        return list(set([ edge[0] for edge in self.edges ]))
+
+    @property
+    def to_node_ids(self) -> list[int]:
+        return list(set([ edge[1] for edge in self.edges ]))
+
+    @property
+    def source_ids(self) -> list[int]:
+        return list(set([ node_id for node_id in self.from_node_ids
+                          if node_id not in self.to_node_ids ]))
+
+    @property
+    def sink_ids(self) -> list[int]:
+        return list(set([ node_id for node_id in self.to_node_ids
+                          if node_id not in self.from_node_ids ]))
+
+    def prune_path(self):
+
+        self.removed_source_ids:list[int] = []
+        while self.source_ids != [ 0 ]:
+            for source_id in self.source_ids:
+                if source_id == 0: continue
+                self.edges = list(filter(lambda e: e[0]!=source_id, self.edges))
+                self.removed_source_ids.append(source_id)
+
+        self.removed_sink_ids:list[int] = []
+        while self.sink_ids != [ self.node_num-1 ]:
+            for sink_id in self.sink_ids:
+                if sink_id == self.node_num-1: continue
+                self.edges = list(filter(lambda e: e[1]!=sink_id, self.edges))
+                self.removed_sink_ids.append(sink_id)
 
     def forward(self, x):
         # for node_id in self.forward_queue:
@@ -73,40 +121,10 @@ class Model(nn.Module):
 
 
 class Node(nn.Module):
-    def __init__(self, node_id, node_num, max_depth, threshold_of_connection,
-                 max_number_of_connection, min_number_of_connection):
+    def __init__(self, node_num, max_depth):
         super(Node, self).__init__()
-        # self.node_id = node_id
-        # self.node_num = node_num
-        # self.max_depth = max_depth
-        # self.alphas = torch.rand(node_num-1)
-        # # self.alphas = torch.ones(node_num-1)
-        # self.passed_nodes:list[bool] = [ False ] * (node_num-1)
-        # self.t_c     = threshold_of_connection
-        # self.max_n_c = max_number_of_connection
-        # self.min_n_c = min_number_of_connection
         self.linear = nn.Linear((node_num-1)*max_depth, max_depth)
         self.activation = nn.ReLU()
-
-    # def get_max_alpha_node_ids(self):
-    #     max_alphas:dict[int, float] = { alpha_id: alpha for alpha_id, alpha in enumerate(self.alphas.numpy()) }
-    #     max_alphas = dict(filter(lambda ma: not self.passed_nodes[ma[0]], max_alphas.items()))
-    #     max_alphas = dict(sorted(max_alphas.items(), key=lambda ma: ma[1], reverse=True))
-    #     max_alphas_above = dict(filter(lambda ma: ma[1] > self.t_c, max_alphas.items()))
-    #     if len(max_alphas_above) > self.min_n_c:
-    #         max_alpha_node_ids = list(max_alphas_above.keys())[:self.max_n_c]
-    #     else:
-    #         max_alpha_node_ids = list(max_alphas.keys())[:self.min_n_c]
-    #     self.max_alpha_node_ids_origin:list[int] = max_alpha_node_ids
-    #     self.max_alpha_node_ids:list[int] = [ mani+1 if mani>=self.node_id else mani for mani in max_alpha_node_ids ]
-
-    # def forward(self, inputs:dict):
-    #     assert set(inputs.keys()) == set(self.max_alpha_node_ids_origin)
-    #     inputs = dict((input[0]-1, input[1]) if input[0]>=self.node_id else input for input in inputs.items())
-    #     x = torch.zeros((self.node_num-1)*self.max_depth)
-    #     for input_id, input_value in inputs.items():
-    #         x[input_id*self.max_depth:(input_id+1)*self.max_depth] = input_value*self.alphas[input_id]
-    #     self.output = self.activation(self.linear(x))
         
     def forward(self, x):
         self.output = self.activation(self.linear(x))
@@ -115,9 +133,28 @@ class Node(nn.Module):
 ''' Execution '''
 if __name__ == "__main__":
 
-    NUMBER_OF_NODES = 90
+    NODE_NUMBER = 32
+    NODE_DEPTH  = 4
 
-    model = Model(NUMBER_OF_NODES, 4, 0.75, 3, 1)
+    model = Model(NODE_NUMBER, NODE_DEPTH, 0.75, 6, 2)
     model.search_path()
     from dag_networkx import plot_DAG
-    plot_DAG(model.edges, NUMBER_OF_NODES)
+    plot_DAG(graph_data={
+        "node_num"          : model.node_num,
+        "edges"             : model.edges,
+        "isolated_node_ids" : model.isolated_node_ids,
+        "source_ids"        : model.source_ids,
+        "removed_source_ids": [],
+        "sink_ids"          : model.sink_ids,
+        "removed_sink_ids"  : [],
+    }, filename="DAG_before_prune")
+    model.prune_path()
+    plot_DAG(graph_data={
+        "node_num"          : model.node_num,
+        "edges"             : model.edges,
+        "isolated_node_ids" : model.isolated_node_ids,
+        "source_ids"        : model.source_ids,
+        "removed_source_ids": model.removed_source_ids,
+        "sink_ids"          : model.sink_ids,
+        "removed_sink_ids"  : model.removed_sink_ids,
+    }, filename="DAG_after_prune")
